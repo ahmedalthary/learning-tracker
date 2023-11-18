360d3b5ed1ef0135de11c28fe68f0168ebfb4de1
ahmedalthary <althryaa2@gmail.com>
2023-11-18 21:04:36 +0300

    Create Register Page

---
+++ client/src/views/Register.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+        <h1> Register</h1>
+        <form @submit.prevent="register">
+            <input v-model="email" type="email" name="email" placeholder="Email">
+            <input v-model="password" type="password" name="password" placeholder="Password">
+            <button type="submit">Register</button>
+        </form>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue';
+import AuthenticationService from '../services/AuthenticationService';
+const email = ref("")
+const password = ref("")
+
+const register = async () => {
+    const response = await AuthenticationService.register({
+        email: email.value,
+        password: password.value
+    })
+    console.log(response.data)
+}
+</script>
+
+<style scoped></style>
--- server/src/app.js
+++ server/src/app.js
@@ -1,1 +1,13 @@
-console.log("hi");
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const app = express();
+
+app.use(morgan("combined"));
+app.use(bodyParser.json());
+app.use(cors());
+app.post("/register", (req, res) => {
+  res.send({ message: `Welcome ${req.body.email} ""Have Fun!` });
+});
+app.listen(process.env.PORT || 3000);
